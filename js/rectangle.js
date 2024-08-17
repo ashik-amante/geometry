b9d73560af3e0466eba1cf1742075b2bfcd913f2
Abdullah Al Ashik <abdullahashik17@gmail.com>
2024-08-17 23:18:59 +0600

    update function bt function

--- js/rectangle.js
+++ js/rectangle.js
@@ -1,19 +1,34 @@
 function rectangleCalculator(){
-    const widthInput = document.getElementById('width');
-    const widthText = widthInput.value;
-    const width = parseFloat(widthText);
+    // const widthInput = document.getElementById('width');
+    // const widthText = widthInput.value;
+    // const width = parseFloat(widthText);
+    const width = getInputValueById('width')
+    const length = getInputValueById('length')
 
     // length
 
-    const lengthInput = document.getElementById('length');
-    const lengthText = lengthInput.value;
-    const length = parseFloat(lengthText);
+    // const lengthInput = document.getElementById('length');
+    // const lengthText = lengthInput.value;
+    // const length = parseFloat(lengthText);
 
     // area
     const area = width * length;
 
     // show result
 
-    const areaResult = document.getElementById('recArea');
-    areaResult.innerText = area;
+    // const areaResult = document.getElementById('recArea');
+    // areaResult.innerText = area;
+    setInnertext('recArea',area)
+}
+
+function getInputValueById(id){
+    const inputField = document.getElementById(id);
+    const inputfieldText = inputField.value;
+    const inputValue = parseFloat(inputfieldText);
+    return inputValue
+}
+
+function setInnertext(id, value){
+    const setValue = document.getElementById(id);
+    setValue.innerText = value;
 }
--- js/rhombus.js
+++ js/rhombus.js
@@ -1,17 +1,33 @@
 function calculateRhombus(){
-    const d1ainput = document.getElementById('d1');
-    const d1Text = d1ainput.value;
-    const d1 = parseFloat(d1Text)
+    // const d1ainput = document.getElementById('d1');
+    // const d1Text = d1ainput.value;
+    // const d1 = parseFloat(d1Text)
+    const d1 = setInputValurById('d1');
+    const d2 = setInputValurById('d2')
 
     // d2
 
-    const d2ainput = document.getElementById('d2');
-    const d2Text = d2ainput.value;
-    const d2 = parseFloat(d2Text)
+    // const d2ainput = document.getElementById('d2');
+    // const d2Text = d2ainput.value;
+    // const d2 = parseFloat(d2Text)
 
     const area = d1 * d2 * 0.5;
 
     // set valuee
-    const RhombusArea = document.getElementById('RhombusArea');
-    RhombusArea.innerText = area
+    // const RhombusArea = document.getElementById('RhombusArea');
+    // RhombusArea.innerText = area
+    setInnertext('RhombusArea' , area)
+}
+
+function setInputValurById(id){
+    const inputField  = document.getElementById(id);
+    const inputfieldText = inputField.value;
+    const inputValue = parseFloat(inputfieldText);
+    return inputValue;
+}
+
+function setInnertext(id, value){
+    const setValue = document.getElementById(id);
+    setValue.innerText = value;
+
 }
